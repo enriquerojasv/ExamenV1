f25867defa8157c12b646019e92a77fb44507a22
enriquerojasv <erojasv@cifpfbmoll.eu>
2020-12-03 18:55:17 +0100

    v3

--- app/src/main/java/com/androidclase/examenv1/MainActivity.java
+++ app/src/main/java/com/androidclase/examenv1/MainActivity.java
@@ -1,20 +1,38 @@
 package com.androidclase.examenv1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment1 frg1;
+    Fragment2 frg2;
+    Fragment3 frg3;
+    FragmentTransaction transaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frg1 = new Fragment1();
+        frg2 = new Fragment2();
+        frg3 = new Fragment3();
+
+        FragmentManager manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_1, frg1, "Frag1");
+        transaction.add(R.id.fragment_2, frg2, "Frag2");
+        transaction.add(R.id.fragment_3, frg3, "Frag3");
+        transaction.commit();
     }
 
     public void requestToChangeText2(String text, int textSize) {
-        System.out.println("activity" + text + textSize);
 
-        //Fragment2 frag2 = (Fragment2) findViewById(R.id.fragment_2);
+        frg2.tuneText(text, textSize);
+
     }
 }
